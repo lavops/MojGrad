fe1b32357e457f9ff63409b94966dedff6ba31db
Djordje Milicevic <milicevic.djordje@hotmail.com>
2020-03-19 11:26:44 +0100

    Izmena za objavu post-a, dodato biranje kamere ili uzimanje slike iz galerije.

--- App/Frontend/lib/ui/CameraPage.dart
+++ App/Frontend/lib/ui/CameraPage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/models/categories.dart';
+import 'package:frontend/ui/homePage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class CameraPage extends StatefulWidget {
   @override
@@ -10,14 +13,30 @@ class _CameraPageState extends State<CameraPage> {
   
   int _vrstaObjave = 1;
   int _problemResava = 1;
-  int _kategorijaProblema = 1;
   List<Category> _categories = Category.getCategories();
   Category category;
+  File imageFile;
+
+  _openGalery() async{
+    var picture = await ImagePicker.pickImage(source: ImageSource.gallery);
+    this.setState(() {
+      imageFile = picture;
+    });
+  }
+
+  _openCamera() async{
+    var picture = await ImagePicker.pickImage(source: ImageSource.camera);
+    this.setState(() {
+      imageFile = picture;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     
     final vrstaObjave = Row(
       children: <Widget>[
+        Align(alignment: Alignment.topLeft, child: Text("Vrsta objave: ", style: TextStyle(fontWeight: FontWeight.bold),)),
         Text("Problem"),
         Radio(
           value: 1,
@@ -27,6 +46,8 @@ class _CameraPageState extends State<CameraPage> {
               _vrstaObjave = value;
             });
           },
+          focusColor: Colors.green[800],
+          activeColor: Colors.green[800],
         ),
         Text("Pohvala"),
         Radio(
@@ -37,13 +58,16 @@ class _CameraPageState extends State<CameraPage> {
               _vrstaObjave = value;
             });
           },
+          focusColor: Colors.green[800],
+          activeColor: Colors.green[800],
         ),
       ],
     );
 
     final problemResava = Row(
       children: <Widget>[
-        Text("Resicu sam problem"),
+        Align(alignment: Alignment.topLeft, child: Text("Problem resava: ", style: TextStyle(fontWeight: FontWeight.bold))),
+        Text("Resicu sam"),
         Radio(
           value: 1,
           groupValue: _problemResava,
@@ -52,8 +76,10 @@ class _CameraPageState extends State<CameraPage> {
               _problemResava = value;
             });
           },
+          focusColor: Colors.green[800],
+          activeColor: Colors.green[800],
         ),
-        Text("Neka neko drugi resi"),
+        Text("Neko drugi"),
         Radio(
           value: 2,
           groupValue: _problemResava,
@@ -62,39 +88,114 @@ class _CameraPageState extends State<CameraPage> {
               _problemResava = value;
             });
           },
+          focusColor: Colors.green[800],
+          activeColor: Colors.green[800],
         ),
       ],
     );
 
-    final _dropDown = DropdownButton<Category>(
-      hint:  Text("Izaberi"),
-      value: category,
-      onChanged: (Category Value) {
-        setState(() {
-          category = Value;
-        });
-      },
-      items: _categories.map((Category option) {
-        return  DropdownMenuItem<Category>(
-          value: option,
-          child: Text(option.name),
-        );
-      }).toList(),
+    final _dropDown = Row(
+      children: <Widget>[
+        Align(alignment: Alignment.topLeft, child: Text("Kategoriju: ",style: TextStyle(fontWeight: FontWeight.bold))),
+        DropdownButton<Category>(
+          hint:  Text("Izaberi"),
+          value: category,
+          onChanged: (Category value) {
+            setState(() {
+              category = value;
+            });
+          },
+          items: _categories.map((Category option) {
+            return  DropdownMenuItem<Category>(
+              value: option,
+              child: Text(option.name),
+            );
+          }).toList(),
+        ),
+      ],
     );
     
+    final cameraPhone = RaisedButton(
+      child: Text('Kamera'),
+      onPressed: (){
+        _openCamera();
+      },
+    );
+
+    final cameraGalery = RaisedButton(
+      child: Text('Galerija'),
+      onPressed: (){
+        _openGalery();
+      },
+    );
+
+    final izaberiKameru = Row(
+      children: <Widget>[
+        Expanded(
+          child: cameraPhone,
+          flex: 2,
+        ),
+        Expanded(
+          child: Container(color: Colors.white),
+          flex: 1,
+        ),
+        Expanded(
+          child: cameraGalery,
+          flex: 2,
+        ),
+      ],
+    );
+
+    final opis = TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.black)
+        ),
+        hintText: (_vrstaObjave == 1)? 'Opis problema': 'Opis pohvale',
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(4)),
+          borderSide: BorderSide(width: 2,color: Colors.green[800]),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(4)),
+          borderSide: BorderSide(width: 1,color: Colors.green[800]),
+        ),
+      ),
+    );
+
+    final submitObjavu = RaisedButton(
+      child: Text('Obajavi'),
+      onPressed: (){
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => MyBottomBar()),
+        );
+      },
+    );
+
     return Center(
       child:Container(
         width: 400,
-        child: Column(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 24.0, right: 24.0, top: 24.0, bottom: 24.0),
           children: <Widget>[
-            Align(alignment: Alignment.topLeft, child: Text("Izaberi vrstu objave: ")),
             vrstaObjave,
             if(_vrstaObjave == 1)...[
-              Align(alignment: Alignment.topLeft, child: Text("Ko ce da resi problem: ")),
               problemResava,
-              Align(alignment: Alignment.topLeft, child: Text("Izaberi kategoriju: ")),
               _dropDown
             ],
+            SizedBox(height: 20.0,),
+            Align(alignment: Alignment.topCenter, child: Text("Izaberi fotografiju: ",style: TextStyle(fontWeight: FontWeight.bold))),
+            Text("Izaberi fotografiju: ",style: TextStyle(fontWeight: FontWeight.bold)),
+            SizedBox(height: 20.0,),
+            izaberiKameru,
+            if(imageFile != null)
+              Image.file(imageFile,width: 300,height: 300,),
+            SizedBox(height: 20.0,),
+            opis,
+            SizedBox(height: 20.0,),
+            submitObjavu
           ],
         ),
       )
