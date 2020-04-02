6a30f7461bd77432e29dfd9e094e8c91e3153a30
branko.ilic <brankoilic98@gmail.com>
2020-04-02 12:31:56 +0200

    no message

--- App/Frontend/frontend_web/lib/services/api.services.dart
+++ App/Frontend/frontend_web/lib/services/api.services.dart
@@ -52,17 +52,6 @@ class APIServices
     return await http.get(serverURL + 'City');
   }
 
-  //returns user with specific id for deletion
-  static Future deleteUser(int id) async {
-    String url = serverURL + 'User/Delete'+ id.toString();
-    return await http.post(url, headers: header, body: convert.jsonEncode({ 'id' : id, }));
-  }
-
-  //returns post with specific id for deletion
-  static Future deletePost(int id) async {
-    String url = serverURL + 'Post/Delete'+ id.toString();
-    
-    return await http.post(url, headers: header,body: jsonEncode({ 'id': id, }),);
-  }
+  
 
 }
