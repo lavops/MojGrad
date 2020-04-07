53547dac3d0f251b24d1d64225f49c390f2be757
Djordje Milicevic <milicevic.djordje@hotmail.com>
2020-04-07 02:17:22 +0200

    Izmenjen profil

--- App/Frontend/frontend_app/lib/ui/user_profile_page.dart
+++ App/Frontend/frontend_app/lib/ui/user_profile_page.dart
@@ -6,6 +6,7 @@ import 'package:frontend/models/user.dart';
 import 'package:frontend/ui/login.dart';
 import 'package:frontend/widgets/circleImageWidget.dart';
 import 'package:frontend/widgets/postWidget.dart';
+import 'package:frontend/widgets/userInfoWidget.dart';
 import 'edit_profile_page.dart';
 import 'homePage.dart';
 
@@ -21,6 +22,7 @@ class UserProfilePage extends StatefulWidget {
 }
 
 class HeaderSection extends State<UserProfilePage> {
+  ScrollController _scrollController;
   User user;
   HeaderSection(User user1) {
     user = user1;
@@ -57,40 +59,24 @@ class HeaderSection extends State<UserProfilePage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
-        title: Text('Profil'),
         elevation: 0,
-        backgroundColor: green,
-        leading: IconButton(
-            icon: Icon(Icons.arrow_back_ios),
-            onPressed: () {
-              String jwt;
-              APIServices.jwtOrEmpty().then((res) {
-                setState(() {
-                  jwt = res;
-                });
-                if (res != null) {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => HomePage.fromBase64(res)));
-                }
-              });
-            }),
+        iconTheme: IconThemeData(color: Colors.black),
+        backgroundColor: Colors.grey[50],
       ),
       endDrawer: Drawer(
         child: ListView(
           children: <Widget>[
             DrawerHeader(
               decoration: BoxDecoration(
-                color: green,
+                color: Colors.grey[50],
               ),
-              child: Text(
+              child: Center(child: Text(
                 user.firstName + " " + user.lastName,
                 style: TextStyle(
-                    color: Colors.white,
+                    color: Colors.black,
                     fontWeight: FontWeight.bold,
-                    fontSize: 24),
-              ),
+                    fontSize: 20),
+              ),)
             ),
             ListTile(
               leading: Icon(Icons.edit, color: Colors.black),
@@ -126,136 +112,24 @@ class HeaderSection extends State<UserProfilePage> {
           ],
         ),
       ),
-      body: Stack(
-        children: <Widget>[
-          Column(children: <Widget>[
-            Container(
-              padding: EdgeInsets.only(top: 3, bottom: 16),
-              width: MediaQuery.of(context).size.width,
-              height: 220,
-              decoration: BoxDecoration(
-                color: green,
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(16),
-                    bottomRight: Radius.circular(16)),
+      body: (user != null)?NestedScrollView(
+          controller: _scrollController,
+          headerSliverBuilder: (BuildContext context, bool boxIsScrolled) {
+            return <Widget>[
+              // User information section
+              SliverToBoxAdapter(child: UserInfoWidget(user)),
+            ];
+          },
+          body: (posts != null)?
+            PostWidget(posts):
+            Center(child: CircularProgressIndicator(
+              valueColor:new AlwaysStoppedAnimation<Color>(Colors.green[800]),
               ),
-              child: Column(
-                children: <Widget>[
-                  Row(children: <Widget>[
-                    CircleImage(
-                      serverURLPhoto + user.photo,
-                      imageSize: 90.0,
-                      whiteMargin: 2.0,
-                      imageMargin: 20.0,
-                    ),
-                    Center(
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            Padding(
-                              padding: EdgeInsets.only(top: 10, left: 70),
-                              child: Text(
-                                user.username,
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 16),
-                              ),
-                            ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.only(top: 10.0, left: 70),
-                              child: Text(
-                                user.firstName + " " + user.lastName,
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 24),
-                              ),
-                            ),
-                          ]),
-                    ),
-                  ]),
-                  Padding(
-                      padding:
-                          const EdgeInsets.only(left: 8, right: 16, top: 25),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Row(
-                            children: <Widget>[
-                              Text(
-                                //postCount,
-                                user.postsNum.toString(),
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              SizedBox(width: 16),
-                              Column(
-                                children: <Widget>[
-                                  Icon(Icons.image,
-                                      color: Colors.white, size: 36),
-                                  Text(
-                                    'Objave',
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: <Widget>[
-                              Text(
-                                user.points.toString(),
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              SizedBox(width: 16),
-                              Column(
-                                children: <Widget>[
-                                  Icon(Icons.check,
-                                      color: Colors.white, size: 36),
-                                  Text(
-                                    'Poeni',
-                                    style: TextStyle(color: Colors.white),
-                                  )
-                                ],
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: <Widget>[
-                              Text(
-                                user.level.toString(),
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              SizedBox(width: 16),
-                              Column(
-                                children: <Widget>[
-                                  Icon(Icons.star_border,
-                                      color: Colors.white, size: 36),
-                                  Text(
-                                    'Nivo',
-                                    style: TextStyle(color: Colors.white),
-                                  )
-                                ],
-                              )
-                            ],
-                          ),
-                        ],
-                      )),
-                ],
-              ),
-            ),
-            Expanded(
-                child: (posts != null)
-                    ? PostWidget(posts)
-                    : Center(
-                        child: CircularProgressIndicator(
-                          valueColor: new AlwaysStoppedAnimation<Color>(
-                              Colors.green[800]),
-                        ),
-                      ))
-          ]),
-        ],
-      ),
+            )
+        ):Center(child: CircularProgressIndicator(
+          valueColor:new AlwaysStoppedAnimation<Color>(Colors.green[800]),
+          ),
+        ),
     );
   }
 }
