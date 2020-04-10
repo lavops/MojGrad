3381e1cade5ae9a3c9a083c365993b4dcd3e7e77
Isidora Arandjelovic <arandjelovic_ika@hotmail.com>
2020-04-10 11:58:04 +0200

    Odradjen UI za filtere

---
+++ App/Frontend/frontend_app/lib/models/filters.dart
@@ -0,0 +1,13 @@
+class Filteri{
+  static const String kategorije = 'Kategorije';
+  static const String gradovi = 'Gradovi';
+  static const String brojlajkova = 'Broj lajkova';
+  static const String vreme = 'Vreme';
+
+  static const List<String> choices = <String>[
+    kategorije,
+    gradovi,
+    brojlajkova,
+    vreme
+  ];
+}
