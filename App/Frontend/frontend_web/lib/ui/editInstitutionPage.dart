98866aa892039fc4cb01f2867b213bb5143a2cb2
Bojan <bojanrajcic25@gmail.com>
2020-04-24 00:03:27 +0200

    Izmenjen edit 2

--- App/Frontend/frontend_web/lib/ui/editInstitutionPage.dart
+++ App/Frontend/frontend_web/lib/ui/editInstitutionPage.dart
@@ -638,6 +638,12 @@ class _EditInstitutionPageState extends State<EditInstitutionPage> {
     final emailRegex = RegExp(r'^[a-z0-9._]{2,}[@][a-z]{3,6}[.][a-z]{2,3}$');
     final mobRegex = RegExp(r'^06[0-9]{7,8}$');
 
+    if (nname == institution.name && ndescription == institution.description && nemail == institution.email && nmobile == institution.phone && cityId == 0 && pass1 == "" && pass2 == "") {
+      setState(() {
+        wrongRegText = "Nista niste izmenili.";
+      });
+      throw Exception("Nista niste izmenili");
+    }
     if (ncityId == 0) {
       ncityId = institution.cityId;
     }
