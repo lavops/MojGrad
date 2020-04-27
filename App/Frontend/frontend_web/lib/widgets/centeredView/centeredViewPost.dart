b6b1b07c9316503aaee4eddb02cf118f167ad64a
Djordje Milicevic <milicevic.djordje@hotmail.com>
2020-04-28 00:04:44 +0200

    Odradjeno upravljanje postovima na dekstop aplikaciji. Dodata nova stranica za upravljanje i novi widget za post (stari ce uskro biti izbrisani).

---
+++ App/Frontend/frontend_web/lib/widgets/centeredView/centeredViewPost.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+
+class CenteredViewPost extends StatelessWidget{
+  final Widget child;
+  const CenteredViewPost({Key key, this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenTypeLayout(
+      mobile: CenteredViewPostMobile(child: child),
+      tablet: CenteredViewPostTabletDesktop(child: child),
+    );
+  }
+}
+
+class CenteredViewPostTabletDesktop extends StatelessWidget{
+  final Widget child;
+  const CenteredViewPostTabletDesktop({Key key, this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 100, right: 100, top: 60),
+      alignment: Alignment.topCenter,
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: 600),
+        child: child,
+      ),
+    );
+  }
+}
---
+++ App/Frontend/frontend_web/lib/widgets/post/singlePostWidget.dart
@@ -0,0 +1,225 @@
+import 'package:flutter/material.dart';
+import 'package:frontend_web/services/api.services.dart';
+import 'package:frontend_web/models/fullPost.dart';
+import 'package:frontend_web/services/token.session.dart';
+import 'package:frontend_web/widgets/circleImageWidget.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+Color greenPastel = Color(0xFF00BFA6);
+
+class SinglePostWidget extends StatefulWidget {
+  FullPost posts;
+
+  SinglePostWidget(this.posts);
+
+  @override
+  _SinglePostWidgetState createState() => _SinglePostWidgetState(posts);
+}
+
+class _SinglePostWidgetState extends State<SinglePostWidget> {
+  FullPost post;
+
+  _SinglePostWidgetState(FullPost post1) {
+    this.post = post1;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return (post == null) ? Center() : newPost(); //buildPostList()
+  }
+
+  showAlertDialog(BuildContext context, int id) {
+      // set up the button
+    Widget okButton = FlatButton(
+      child: Text("Obriši", style: TextStyle(color: Colors.green),),
+      onPressed: () {
+        APIServices.deletePost(TokenSession.getToken,id);
+        setState(() {
+          post = null;
+        });
+        Navigator.pop(context);
+        /*Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => ManagePostPage()),
+        );*/
+        },
+    );
+     Widget notButton = FlatButton(
+      child: Text("Otkaži", style: TextStyle(color: Colors.green),),
+      onPressed: () {
+        Navigator.pop(context);
+        },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("Brisanje objave"),
+      content: Text("Da li ste sigurni da želite da obrišete objavu?"),
+      actions: [
+        okButton,
+        notButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+  Widget newPost() {
+    
+    return Card(
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+        userInfoRow(post.username,
+            post.typeName, post.userPhoto, post.postId),
+        imageGallery(post.photoPath),
+        SizedBox(height: 2.0),
+        actionsButtons(
+            post.statusId,
+            post.postId,
+            post.likeNum,
+            post.dislikeNum,
+            post.commNum),
+        description(
+            post.username, post.description),
+        SizedBox(height: 6.0),
+        ]
+      )
+    );  
+  }
+
+  Widget userInfoRow(String username, String category, String userPhoto, int postId) => Row(
+        children: <Widget>[
+          CircleImage(
+            userPhotoURL + userPhoto,
+            imageSize: 36.0,
+            whiteMargin: 2.0,
+            imageMargin: 6.0,
+          ),
+          Text(
+            username,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Expanded(child: SizedBox()),
+          Text(category),
+          SizedBox(width: 15,),
+          FlatButton(
+            shape: RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(11.0),
+                side: BorderSide(color: Colors.redAccent)),
+            color: Colors.redAccent,
+            child: Text(
+              "Obriši objavu",
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: () {
+              showAlertDialog(context, postId);
+            },
+          ),
+          SizedBox(width: 15,)
+        ],
+      );
+
+  Widget imageGallery(String image) => Container(
+        constraints: BoxConstraints(
+          maxHeight: 400.0, // changed to 400
+          minHeight: 200.0, // changed to 200
+          maxWidth: double.infinity,
+          minWidth: double.infinity,
+        ),
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: Colors.grey[200],
+              width: 1.0,
+            ),
+          ),
+        ),
+        child: Image(image: NetworkImage(userPhotoURL + image)),
+      );
+
+  Widget actionsButtons(
+          int statusId, int postId, int likeNum, int dislikeNum, int commNum) =>
+      Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          // Actions buttons/icons
+          Row(
+            children: <Widget>[
+              IconButton(
+                icon: Icon(MdiIcons.thumbUpOutline, color: greenPastel),
+                onPressed: () {
+                  APIServices.addLike(TokenSession.getToken,postId, 1, 2);
+                },
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Text(likeNum.toString()),
+              ),
+              IconButton(
+                icon: Icon(MdiIcons.thumbDownOutline, color: Colors.red),
+                onPressed: () {
+                  APIServices.addLike(TokenSession.getToken,postId, 1, 1);
+                },
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Text(dislikeNum.toString()),
+              ),
+              IconButton(
+                icon: Icon(Icons.chat_bubble_outline, color: greenPastel),
+                onPressed: () {
+                },
+              ),
+              Text(commNum.toString()),
+              Expanded(child: SizedBox()),
+              statusId == 2
+                  ? FlatButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(11.0),
+                          side: BorderSide(color: greenPastel)),
+                      color: greenPastel,
+                      child: Text(
+                        "Reši",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {},
+                    )
+                  : IconButton(
+                      icon: Icon(Icons.done_all, color: greenPastel),
+                      onPressed: () {},
+                    ),
+              SizedBox(width: 10.0), // For padding
+            ],
+          ),
+        ],
+      );
+
+  Widget description(String username, String description) => Container(
+          child: Row(
+        children: <Widget>[
+          SizedBox(
+            width: 10,
+          ),
+          Text(username, style: TextStyle(fontWeight: FontWeight.bold)),
+          SizedBox(
+            width: 10,
+          ),
+          Flexible(
+            child: Text(description),
+          )
+        ],
+      ));
+}
