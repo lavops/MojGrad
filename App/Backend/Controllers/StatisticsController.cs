d6c4c405041975161d34b08a98ceb84827bf4cf9
Aleksandra Kazakovic <aleksandrakazakovic@gmail.com>
2020-05-05 21:55:04 +0200

    statistika u backendu

---
+++ App/Backend/Controllers/StatisticsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend.Models.ViewsModel;
+using Backend.UI.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly IStatisticsUI _iStatisticsUI;
+
+        public StatisticsController(IStatisticsUI iStatisticsUI)
+        {
+            _iStatisticsUI = iStatisticsUI;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public StatisticsViewModel GetStatistics()
+        {
+            return _iStatisticsUI.basicStatistics();
+        }
+    }
+}
